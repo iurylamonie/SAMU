7c27a9750b35425e604f38a84673dd157e264c6c
Iury Lamonie <iury.lamonie@hotmail.com>
2017-01-07 17:46:51 -0200

    Finalizando Ocorrência

--- Servico/REST/Controllers/OcorrenciaController.cs
+++ Servico/REST/Controllers/OcorrenciaController.cs
@@ -54,5 +54,48 @@ namespace REST.Controllers
             return ocorrencias.ToList();
         }
 
+        [AcceptVerbs("GET")]
+        [Route("ListarFinalizados/{usuario_id}")]
+        public List<Models.Ocorrencia> ListarFinalizados(int usuario_id)
+        {
+            Models.SAMUDataContext sdc = new Models.SAMUDataContext();
+            var ocorrencias = from o in sdc.Ocorrencias
+                              where o.usuario_id == usuario_id && o.situacao == "Finalizado"
+                              select o;
+            return ocorrencias.ToList();
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("Alterar/{id}")]
+        public void Alterar(int id, [FromBody] string conteudo)
+        {
+            Models.Ocorrencia putOcorrencia = JsonConvert.DeserializeObject<Models.Ocorrencia>(conteudo);
+            Models.SAMUDataContext sdc = new Models.SAMUDataContext();
+            var r = (from o in sdc.Ocorrencias
+                     where o.id == id
+                     select o).Single();
+
+            r.nomeSolicitante = putOcorrencia.nomeSolicitante;
+            r.nomeVitima = putOcorrencia.nomeVitima;
+            r.quantidadeVitimas = putOcorrencia.quantidadeVitimas;
+            r.endereco = putOcorrencia.endereco;
+            r.cep = putOcorrencia.cep;
+            r.informacaoAdicional = putOcorrencia.informacaoAdicional;
+            r.tipo = putOcorrencia.tipo;
+
+            sdc.SubmitChanges();
+        }
+
+        [AcceptVerbs("DELETE")]
+        [Route("Deletar/{id}")]
+        public void Deletar(int id)
+        {
+            Models.SAMUDataContext sdc = new Models.SAMUDataContext();
+            var r = (from o in sdc.Ocorrencias
+                     where o.id == id
+                     select o).Single();
+            sdc.Ocorrencias.DeleteOnSubmit(r);
+            sdc.SubmitChanges();
+        }
     }
 }
